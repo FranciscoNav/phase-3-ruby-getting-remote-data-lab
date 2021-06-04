693e1779cee73a881596acfbaf2d1eac43d9afc0
Francisco <fnaveira11@gmail.com>
2021-06-04 15:07:02 -0400

    commit tree ae13114cadec67fe0ba68aaecf1ef9a6322ab685

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -5,10 +5,12 @@ require 'json'
 
 class GetRequester
 
-    URL = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
+    def initialize(url)
+        @url = url
+    end
 
     def get_response_body
-        uri = URI.parse(URL)
+        uri = URI.parse(@url)
         response = Net::HTTP.get_response(uri)
         response.body
     end
@@ -17,7 +19,7 @@ class GetRequester
         # we use the JSON library to parse the API response into nicely formatted JSON
         programs = JSON.parse(self.get_response_body)
         programs.collect do |program|
-          program["name"]  
+          program
         end
     end 
 
@@ -26,5 +28,5 @@ end
 # programs = GetRequester.new.get_response_body
 # puts programs
 
-programs = GetRequester.new
-puts programs.parse_json.uniq 
+# programs = GetRequester.new
+# puts programs.parse_json.uniq 
